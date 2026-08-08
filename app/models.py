45658623f95ae0a9f3b8eb1608d30e5799169008
wu <democritushygge@gmail.com>
2026-08-08 18:08:54 +0200

    fix file name typo and id Column datatype in model

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from .db import Base
 
 class Item(Base):
     __tablename__ = "items"
-    id = Column(Integer, primary_key=True)
+    id = Column(String, primary_key=True)
     source = Column(String)
     type = Column(String)
     grading_mode = Column(String)
